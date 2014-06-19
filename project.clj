8efa91bf68811b18df6c8b131b8c478f2b0014f3
dennis84 <d.dietrich84@gmail.com>
2014-06-19 14:04:02 +0200

    first

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject landing "0.1.0"
+  :description "A landing page generator"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.8"]
+                 [clj-http "0.9.2"]
+                 [hiccup "1.0.5"]
+                 [markdown-clj "0.9.44"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler landing/app})
---
+++ src/landing.clj
@@ -0,0 +1,29 @@
+(ns landing
+  (:use [compojure.core]
+        [markdown.core :only (md-to-html-string)]
+        [hiccup.page :only (html5)])
+  (:require [clj-http.client :as client]
+            [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defn- make-github-url [user repo path]
+  (str "https://raw.githubusercontent.com/" user "/" repo "/master/" path))
+
+(defn- make-landing-page [html]
+  (html5
+    [:head [:title ""]]
+    [:body html]))
+
+(defn landing-page [user repo]
+  (let [get-file (partial make-github-url user repo)
+        resp (client/get (get-file "README.md"))
+        markdown (:body resp)
+        html (md-to-html-string markdown)]
+    (make-landing-page html)))
+
+(defroutes app-routes
+  (GET "/:user/:repo" [user repo] (landing-page user repo))
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
