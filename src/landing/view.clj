5b06b4336d7ef3b95dd96f6384cd26ce30217882
dennis84 <d.dietrich84@gmail.com>
2014-06-21 12:47:15 +0200

    solarized

--- src/landing/view.clj
+++ src/landing/view.clj
@@ -5,6 +5,7 @@
   (html5
     [:head [:title ""]
            (include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css")
+           (include-css "/css/solarized-dark.css")
            (include-css "/css/index.css")
            (include-js "//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js")
            (include-js "/js/landing.js")]
