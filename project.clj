598ef9b7213745b1438fe8e2f06a13c662a93d1b
dennis84 <d.dietrich84@gmail.com>
2014-06-22 13:22:36 +0200

    removed frontend stuff

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.3.0"]
-                 [clj-http "0.9.2"]
-                 [hiccup "1.0.5"]]
+                 [clj-http "0.9.2"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler landing/app}
   :uberjar-name "landing-standalone.jar"
--- src/landing.clj
+++ src/landing.clj
@@ -1,34 +1,37 @@
 (ns landing
   (:use [compojure.core]
-        [landing.markdown]
-        [ring.util.response :only (redirect)])
+        [landing.markdown])
   (:require [clj-http.client :as client]
             [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
-            [landing.view :as view]
             [landing.templating :as tpl]))
 
-(defn- make-github-url [user repo path]
-  (str "https://raw.githubusercontent.com/" user "/" repo "/master/" path))
+(defn- make-github-url [user repo branch path]
+  (str "https://raw.githubusercontent.com/" user "/" repo "/" branch "/" path))
+
+(defn- get-theme []
+  (:body (client/get (make-github-url "dennis84" "landing-theme" "gh-pages" "index.html"))))
+
+(defn- get-readme [user repo]
+  (:body (client/get (make-github-url user repo "master" "README.md"))))
+
+(defn- homepage [] "Usage: username/repo")
+
+(defn- not-found [] "Not found")
 
 (defn- landing-page [user repo]
   (try
-    (let [theme (:body (client/get (make-github-url "dennis84" "landing-theme" "index.html")))
-          markdown (:body (client/get (make-github-url user repo "README.md")))
-          html (parse-markdown markdown)]
-      (tpl/replace-params theme {:title "" :body html}))
-    (catch Exception e (view/not-found user repo))))
+    (tpl/replace-params (get-theme)
+      {:title "" :body (parse-markdown (get-readme user repo))})
+    (catch Exception e (not-found))))
 
 (defroutes app-routes
-  (route/resources "/" {:root ""})
-  (GET "/" [] (view/homepage))
-  (POST "/" [repo] (redirect repo))
+  (GET "/" [] (homepage))
   (GET "/:user/:repo" [user repo] (landing-page user repo))
-  (route/not-found (view/homepage)))
+  (route/not-found (not-found)))
 
-(def app
-  (handler/site app-routes))
+(def app (handler/site app-routes))
 
 (defn -main [port]
   (jetty/run-jetty app {:port (Integer. port) :join? false}))
