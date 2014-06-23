3222feb8c3b03147bf0c852be1ae30b5a08b444d
dennis84 <d.dietrich84@gmail.com>
2014-06-23 19:27:32 +0200

    homepage

--- src/landing.clj
+++ src/landing.clj
@@ -5,6 +5,7 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
+            [ring.util.response :as resp]
             [landing.templating :as tpl]))
 
 (defn- get-theme []
@@ -15,20 +16,20 @@
         url (str "https://api.github.com/repos/" user "/" repo "/readme?access_token=" token)]
     (:body (client/get url {:accept "application/vnd.github.VERSION.raw"}))))
 
-(defn- homepage [] "Usage: username/repo")
-
-(defn- not-found [] "Not found")
+(defn- homepage []
+  (resp/resource-response "index.html" {:root ""}))
 
 (defn- landing-page [user repo]
   (try
     (tpl/replace-params (get-theme)
-      {:title "" :body (parse-markdown (get-readme user repo))})
-    (catch Exception e (not-found))))
+      {:user user :repo repo :body (parse-markdown (get-readme user repo))})
+    (catch Exception e (homepage))))
 
 (defroutes app-routes
   (GET "/" [] (homepage))
   (GET "/:user/:repo" [user repo] (landing-page user repo))
-  (route/not-found (not-found)))
+  (route/resources "/" {:root ""})
+  (route/not-found (homepage)))
 
 (def app (handler/site app-routes))
 
